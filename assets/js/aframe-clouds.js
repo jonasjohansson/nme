71628ae65a157aa87387c6aa705bbb7299be5258
Jonas Johansson <jns.johansson@gmail.com>
2020-07-12 08:27:16 +0200

    update

--- assets/js/aframe-clouds.js
+++ assets/js/aframe-clouds.js
@@ -1,12 +1,12 @@
 AFRAME.registerComponent('clouds', {
-	init() {
-		const scene = this.el.sceneEl.object3D;
-		const renderer = this.el.sceneEl.renderer;
-		this.cloudShader = new CloudShader(renderer, 512);
-		this.cloudShader.cloudMesh.scale.multiplyScalar(4.0);
-		scene.add(this.cloudShader.cloudMesh);
-	},
-	tick() {
-		this.cloudShader.update();
-	}
-});
+    init() {
+        const scene = this.el.sceneEl.object3D
+        const renderer = this.el.sceneEl.renderer
+        this.cloudShader = new CloudShader(renderer, 512)
+        this.cloudShader.cloudMesh.scale.multiplyScalar(2.0)
+        scene.add(this.cloudShader.cloudMesh)
+    },
+    tick() {
+        this.cloudShader.update()
+    }
+})
